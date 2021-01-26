41f93f92fd9cc30adca9974e4d6e304391b1839b
msolorio <msolorio.github@gmail.com>
2021-01-26 12:10:05 -0800

    Adds Lecture notes for Variables, Strings, and Booleans

---
+++ lecture-notes.py
@@ -0,0 +1,69 @@
+####################################################
+# VARIABLES
+
+shop_name = 'Tribute Pizza'
+total_pizzas = 12.0
+store_is_open = True
+store_is_open = False
+pizzas_by_the_slice = ['cheese', 'supreme', 'pepperoni']
+order = {
+  'name': 'Chris',
+  'price': 15.5
+}
+
+# print(shop_name)
+# print(total_pizzas)
+# print(store_is_open)
+
+# print(type(shop_name))
+# print(type(total_pizzas))
+# print(type(pizzas_by_the_slice))
+# print(type(order))
+
+####################################################
+# COMPARISON OPERATORS
+
+# print(shop_name != 'Maria\'s Pizzas')
+
+# > < <= >= == !=
+# print(total_pizzas < 10)
+
+# # + - * / %
+# print(total_pizzas * 3)
+# print(total_pizzas // 2)
+# print(6.0 == 6)
+
+####################################################
+# STRINGS
+person_name = 'Philip'
+last_name = 'Smith'
+dogs_name = 'Spot'
+
+greeting = f'Hello, my name is {person_name} {last_name}'
+
+# print(greeting)
+
+new_greeting = greeting + f'and my dog\'s name is {dogs_name}'
+# print(new_greeting)
+
+#####################################################
+# BOOLEANS
+
+it_is_monday = False
+it_is_tuesday = False
+it_is_cloudy = True
+
+# JavaScript - && ||
+# Python - and or not
+# print(not it_is_monday)
+# print(not it_is_tuesday and it_is_cloudy)
+# print(it_is_monday or it_is_tuesday)
+
+# JavaScript - 0, undefined, null, '', NaN, false
+# Python - 0, 0.0, '', None, [], {}, False
+
+# print('Monday' or 'Tuesday')
+# print('Monday' and '')
+
+# Print statement will only run if it_is_tuesday is True
+# it_is_tuesday and print('Ordering Pizza Dough')

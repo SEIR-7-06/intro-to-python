eb0b03025a6d52d3ad5d94648fd94d0dc573413e
msolorio <msolorio.github@gmail.com>
2021-01-26 14:12:17 -0800

    Adds conditional logic, loops, and functions to lecture notes

--- lecture-notes.py
+++ lecture-notes.py
@@ -65,5 +65,79 @@ it_is_cloudy = True
 # print('Monday' or 'Tuesday')
 # print('Monday' and '')
 
-# Print statement will only run if it_is_tuesday is True
 # it_is_tuesday and print('Ordering Pizza Dough')
+
+#####################################################
+# CONDITIONAL LOGIC
+
+"""
+in JavaScript
+if (pet === 'dog) {
+  console.log('You will need to train your pet')
+} else if (pet === 'cat') {
+
+} else {
+
+}
+"""
+
+# pet = 'dog'
+# age = 3
+
+# if pet == 'dog':
+#   print('You will need to train your pet')
+# elif age == 3:
+#   print('You won\'t need to train your pet too much')
+# else:
+#   print('We don\'t have that pet in our records')
+
+# pet == 'dog' and print('You will need to train your pet')
+
+#####################################################
+# LOOPS
+
+# count = 0
+
+# while count < 5:
+#   print(count, 'is less than 5')
+#   count += 1
+
+# count = 15
+
+# for number in range(1, count + 1):
+#   if number % 2 == 0:
+#     print(number)
+# else:
+#   print('done')
+
+"""
+JavaScript
+
+function getGreeting(personName) {
+  return `Hello, my name is ${personName}`;
+}
+"""
+
+def get_greeting(person_name):
+  return f'Hello, nice to meet you. My name is {person_name}'
+
+greeting = get_greeting('Michael')
+
+# print(greeting)
+
+"""
+You write a function that will count from 1-99 printing 1 of four things for each number. If the number is divisible by 3, print "fizz". If the number is divisible by 5, print "buzz". If the number is divisible by 3 and 5, print "fizzbuzz". And otherwise, just print the number.
+"""
+
+def fizzbuzz(last_number):
+  for i in range(1, last_number + 1):
+    if i % 3 == 0 and i % 5 == 0:
+      print('fizzbuzz')
+    elif i % 3 == 0:
+      print('fizz')
+    elif i % 5 == 0:
+      print('buzz')
+    else:
+      print(i)
+
+fizzbuzz(10)
